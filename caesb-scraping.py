8967fcf75c946603ccf53697c2a5796699da20a9
G4L1T0 <galito.root@gmail.com>
2022-02-05 21:05:15 -0300

    add function post_tweet

--- caesb-scraping.py
+++ caesb-scraping.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from utils.twitter import post_tweet
 
 URL = "https://www.caesb.df.gov.br/portal-servicos/app/publico/consultarfaltadagua"
+
+# post_tweet('message here')
 
---
+++ utils/twitter.py
@@ -0,0 +1,23 @@
+import os
+import tweepy # https://github.com/tweepy/tweepy/tree/master/examples/API_v2
+from dotenv import load_dotenv
+
+load_dotenv()
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+consumer_secret = os.environ.get('CONSUMER_SECRETKEY')
+consumer_key = os.environ.get('CONSUMER_APIKEY')
+bearer_token = os.environ.get('BEARER_TOKEN')
+access_token = os.environ.get('ACCESS_TOKEN')
+
+
+def post_tweet(message):
+
+    client = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret
+    )
+
+    response = client.create_tweet(text=message)
+    print(response)
